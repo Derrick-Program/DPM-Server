9d80912184000b30238854352f072b8130b90681
derrick <derrick@YENKAIMacBookPro-7.local>
2026-07-28 03:37:05 +0800

    feat: add addsub package source

--- packages/addsub/src/addsub.c
+++ packages/addsub/src/addsub.c
@@ -1,7 +1,2 @@
-int add(int a, int b) {
-    return a + b;
-}
-
-int subtract(int a, int b) {
-    return a - b;
-}
+int add(int a, int b) { return a + b; }
+int subtract(int a, int b) { return a - b; }
